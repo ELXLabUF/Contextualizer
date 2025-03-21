7fa00725fddf8a7b63de81347545594e7bafa989
aakash-naik <aa.naik@ufl.edu>
2025-03-21 12:01:43 -0400

    Changed env variable.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,12 @@
-//export const environment = {
-//    firebase: {
-//        apiKey: "AIzaSyC7DfNKsu4LQ5CvR_moSGOnwfQcCqwaQxM",
-//        authDomain: "contextualizer-e57ed.firebaseapp.com",
-//        projectId: "contextualizer-e57ed",
-//        storageBucket: "contextualizer-e57ed.appspot.com",
-//        messagingSenderId: "829469841315",
-//        appId: "1:829469841315:web:84a34218c225df8a46ec16",
-//        measurementId: "G-C2R3Q3FL14",
-//    },
-//    production: false,
-//};
-
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyBuwMz4r20palw5Pse9Y-iLF0VEfObcfos",
-        authDomain: "exp-page-filter-project.firebaseapp.com",
-        projectId: "exp-page-filter-project",
-        storageBucket: "exp-page-filter-project.appspot.com",
-        messagingSenderId: "975138568454",
-        appId: "1:975138568454:web:4782479d6888dba744c687",
+        apiKey: "AIzaSyC7DfNKsu4LQ5CvR_moSGOnwfQcCqwaQxM",
+        authDomain: "contextualizer-e57ed.firebaseapp.com",
+        projectId: "contextualizer-e57ed",
+        storageBucket: "contextualizer-e57ed.appspot.com",
+        messagingSenderId: "829469841315",
+        appId: "1:829469841315:web:84a34218c225df8a46ec16",
+        measurementId: "G-C2R3Q3FL14",
     },
     production: false,
 };
